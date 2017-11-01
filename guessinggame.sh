6cca736abf9c380063d4308072f38087727f7e65
manca-s <zefiro_it@yahoo.it>
2017-11-01 10:31:11 +0100

    add description

---
+++ guessinggame.sh
@@ -0,0 +1,6 @@
+# File: guessinggame.sh
+# Description: peer-grated assignment for Unix Workbench course (Coursera)
+# To do: 1.  ask to user how many files are in the current directory
+#	 2A. if the answer is correct then they congrat and the pgm stops
+#	 2B. if the answer is incorrect then they advise that the guess is low #	     or high and they prompt to try again
+#	 3.  the pgm doesn't end until the user guess the number of files
