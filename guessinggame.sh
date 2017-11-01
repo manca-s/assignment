35d44a0f8ca0a557dea63c5a3c43ddce5b5ad0f5
manca-s <zefiro_it@yahoo.it>
2017-11-01 16:37:22 +0100

    add input

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,14 @@
+##!/usr/bin/env bash
 # File: guessinggame.sh
 # Description: peer-grated assignment for Unix Workbench course (Coursera)
 # To do: 1.  ask to user how many files are in the current directory
 #	 2A. if the answer is correct then they congrat and the pgm stops
-#	 2B. if the answer is incorrect then they advise that the guess is low #	     or high and they prompt to try again
+#	 2B. if the answer is incorrect then they advise that the guess is low 
+#	     or high and they prompt to try again
 #	 3.  the pgm doesn't end until the user guess the number of files
+
+echo "Hi my friend! My name is GUESSINGGAME and I challenge you."
+echo "How many files are in current directory?"
+read guess
+
+echo "$guess"
